38181c5c54ab8eeb703250203f5ca51c40d4127b
dongjincheng1 <2032088697@qq.com>
2016-07-06 16:35:00 +0800

    djc框架搭载完成

--- app/src/main/java/com/example/beautydress/Activity/MainActivity.java
+++ app/src/main/java/com/example/beautydress/Activity/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.beautydress.Activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.example.beautydress.R;
+import com.example.beautydress.fragment.TabCartFragment;
+import com.example.beautydress.fragment.TabHomeFragment;
+import com.example.beautydress.fragment.TabMineFragment;
+import com.example.beautydress.fragment.TabNineFragment;
 
 public class MainActivity extends AppCompatActivity {
     private RadioGroup rg_guide;
@@ -15,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton rb_9kuai9;
     private RadioButton rb_cart;
     private RadioButton rb_mine;
+    private FragmentTransaction transaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidgets();
+        aboutRadioGroupGuide();
+
 
 
     }
@@ -29,10 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private void  initWidgets(){
         rg_guide=(RadioGroup)findViewById(R.id.rg_guide_id);
         ll_cotainer=(LinearLayout) findViewById(R.id.ll_container_id);
-        rb_home=(RadioButton)findViewById(R.id.rb_home_id);
-        rb_9kuai9=(RadioButton)findViewById(R.id.rb_9kuai9_id);
-        rb_cart=(RadioButton)findViewById(R.id.rb_cart_id);
-        rb_mine=(RadioButton)findViewById(R.id.rb_space_id);
+        transaction=getSupportFragmentManager().beginTransaction();
+        repacleView(new TabHomeFragment());
     }
 
     /**
@@ -44,11 +51,23 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 switch (i){
                     case R.id.rb_home_id:
-
+                        repacleView(new TabHomeFragment());
                         break;
-
+                    case R.id.rb_9kuai9_id:
+                        repacleView(new TabNineFragment());
+                        break;
+                    case R.id.rb_cart_id:
+                        repacleView(new TabCartFragment());
+                        break;
+                    case R.id.rb_space_id:
+                        repacleView(new TabMineFragment());
+                        break;
                 }
             }
         });
     }
+    private void repacleView(Fragment fragment){
+        transaction=getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.ll_container_id,fragment).commit();
+        }
 }
---
+++ app/src/main/java/com/example/beautydress/fragment/TabHomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.beautydress.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.beautydress.R;
+
+/**
+ * Created by Djc on 2016/7/6.
+ */
+public class TabHomeFragment extends Fragment {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=(View) inflater.inflate(R.layout.tab_home_layout,null);
+        return view;
+    }
+}
